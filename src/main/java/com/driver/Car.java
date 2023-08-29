3d84f71890fc8f45032957a5b301ff838ef0065e
Aniket Birambole <aniketb1811@gmail.com>
2023-08-29 13:32:13 +0530

    Commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,7 +11,7 @@ public class Car extends Vehicle {
     private int seats;
 
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type) {
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         this.name = name;
         this.wheels = wheels;
